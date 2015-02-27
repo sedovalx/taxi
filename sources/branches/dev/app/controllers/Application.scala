2d81f924979d52c699c295430f366c8b8572652f
sedovalx <sedov.alx@gmail.com>
2015-02-27 18:27:01 +0300

    мелкие дополнения

--- sources/branches/dev/app/controllers/Application.scala
+++ sources/branches/dev/app/controllers/Application.scala
@@ -4,7 +4,7 @@ import play.api.mvc._
 
 object Application extends Controller {
 
-  def index = Action {
+  def index(path: String) = Action {
     Ok(views.html.index("Your new application is ready."))
   }
 }
