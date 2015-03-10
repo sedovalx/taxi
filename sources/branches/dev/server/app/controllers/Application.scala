aa3b7ed199b8f9a92e75faea12b7e0a6875e895d
sedovalx <sedov.alx@gmail.com>
2015-03-11 00:37:07 +0300

    Поправлен серверный роутинг, чтобы клиентское приложение запускалось и работало из каталога public

--- sources/branches/dev/server/app/controllers/Application.scala
+++ sources/branches/dev/server/app/controllers/Application.scala
@@ -1,10 +1,15 @@
 package controllers
 
+import java.io.File
+
+import play.Play
 import play.api.mvc._
 
 object Application extends Controller {
 
   def index(path: String) = Action {
-    Ok(views.html.index("Your new application is ready."))
+    val projectRoot = Play.application().path()
+    val file = new File(projectRoot + "/public/index.html")
+    Ok(scala.io.Source.fromFile(file.getCanonicalPath).mkString).as("text/html")
   }
 }
