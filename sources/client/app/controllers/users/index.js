f97639f89de08bb1980665f2b28f6b5ddccc0a69
sedovalx <sedov.alx@gmail.com>
2015-03-18 23:44:50 +0300

    #6: список немного причесан, к нему добавлены кнопки комманд.

--- sources/client/app/controllers/users/index.js
+++ sources/client/app/controllers/users/index.js
@@ -1,35 +1,59 @@
 import Ember from "ember";
 
 export default Ember.ArrayController.extend({
+  sortProperties: ["lastName", "firstName", "middleName", "login"],
+  selectedRow: null,
+  selectionEmpty: function(){
+    return this.get("selectedRow") == null;
+  }.property("selectedRow"),
   columns: function(){
     let loginColumn = Ember.Table.ColumnDefinition.create({
       savedWidth: 200,
+      canAutoResize: true,
       headerCellName: "Логин",
       contentPath: "login"
     });
     let lastNameColumn = Ember.Table.ColumnDefinition.create({
       savedWidth: 200,
+      canAutoResize: true,
       headerCellName: "Фамилия",
       contentPath: "lastName"
     });
     let firstNameColumn = Ember.Table.ColumnDefinition.create({
       savedWidth: 200,
+      canAutoResize: true,
       headerCellName: "Имя",
       contentPath: "firstName"
     });
     let middleNameColumn = Ember.Table.ColumnDefinition.create({
       savedWidth: 200,
+      canAutoResize: true,
       headerCellName: "Отчество",
       contentPath: "middleName"
     });
     let roleColumn = Ember.Table.ColumnDefinition.create({
       savedWidth: 200,
+      canAutoResize: true,
       headerCellName: "Роль",
       contentPath: "role"
     });
-    return [loginColumn, lastNameColumn, firstNameColumn, middleNameColumn, roleColumn];
+    return [lastNameColumn, firstNameColumn, middleNameColumn, roleColumn, loginColumn];
   }.property(),
   users: function(){
     return this;
-  }.property("@each")
+  }.property("@each"),
+  actions: {
+    edit: function(){
+      alert(this.get("selectedRow"));
+    },
+    view: function(){
+
+    },
+    create: function(){
+
+    },
+    remove: function(){
+
+    }
+  }
 });
