e71e677bdec5ffc47c776516ba7847c8a5af364a
sedovalx <sedov.alx@gmail.com>
2015-03-20 02:55:58 +0300

    #7: Проверка на несохраненные измененеия при отмене создания пользователя

--- sources/client/app/controllers/users/new.js
+++ sources/client/app/controllers/users/new.js
@@ -1,4 +1,5 @@
 import Ember from "ember";
+import $ from "jquery";
 
 export default Ember.ObjectController.extend({
   roles: [
@@ -7,5 +8,31 @@ export default Ember.ObjectController.extend({
     { id: "Repairman", label: "Ремонтник" },
     { id: "Cashier", label: "Кассир" }
   ],
-  selectedRole: null
+  selectedRole: null,
+  actions: {
+    save: function(){
+      let that = this;
+      this.get("model")
+        .save()
+        .then(() => that.transitionToRoute("users"))
+        .catch(error => {
+          alert(error);
+        });
+    },
+    cancel: function(){
+      this.transitionToRoute("users");
+    }
+  },
+  isDirty: function(){
+    let model = this.get("model");
+    return (model && this._isDirty(model));
+  },
+  discardModel: function(){
+    let model = this.get("model");
+    this.store.unloadRecord(model);
+    this.set("model", null);
+  },
+  _isDirty: function(model){
+    return !$.isEmptyObject(model.changedAttributes());
+  }
 });
