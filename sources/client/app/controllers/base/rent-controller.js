68b5f21e2731ad091f050df7f1804bceeec6b677
sedovalx <sedov.alx@gmail.com>
2015-04-25 15:44:24 +0300

    #41: теперь в выпадающем списке машин только свободные машины

--- sources/client/app/controllers/base/rent-controller.js
+++ sources/client/app/controllers/base/rent-controller.js
@@ -1,4 +1,5 @@
 import Ember from "ember";
+import DS from "ember-data";
 import DirtyControllerMixin from "client/controllers/base/dirty-controller-mixin";
 import statuses from "client/models/rent-statuses";
 
@@ -28,7 +29,19 @@ export default Ember.ObjectController.extend(DirtyControllerMixin, {
     return this.get("statuses").filter(r => r.id === model.get("status"))[0];
   }.property("model"),
   carItems: function() {
-    return this.store.find("car");
+    return DS.PromiseArray.create({
+      promise: Ember.RSVP.all([
+        this.store.find("car"),
+        this.store.find("rent")
+      ]).then(arrays => {
+        let cars = arrays[0];
+        let rents = arrays[1];
+        // выбираем все занятые в открытых арендах машины
+        let busyCarIds = rents.filter(r => r.get("status") !== "Closed").map(r => r.get("car").get("id"));
+        // из всех машин отфильтровываем свободные
+        return cars.filter(c => busyCarIds.indexOf(c.get("id")) < 0);
+      })
+    });
   }.property("model"),
   driverItems: function() {
     return this.store.find("driver");
