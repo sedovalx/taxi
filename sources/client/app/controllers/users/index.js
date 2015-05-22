702caec4e8c6b725fcc819a74ff1780787308525
Argelein <argelein@gmail.com>
2015-05-23 00:11:20 +0300

    #23: Фильтрация пользователей работает, хоть пока по-уродски выглядит.

--- sources/client/app/controllers/users/index.js
+++ sources/client/app/controllers/users/index.js
@@ -1,12 +1,46 @@
 import Ember from "ember";
+import roles from "client/models/roles";
 
 export default Ember.ArrayController.extend({
+  queryParams: ['login', 'role','lastName','firstName','middleName'],
   sortProperties: ["lastName", "firstName", "middleName", "login"],
   selectedRow: null,
+
+  //roles model
+  roles: roles,
+  //let allRoles = roles;
+  //allRoles.push({id: '', label: 'Все'});
+  //  return roles;
+  //},
+  allRoles: [ { id: 0, label: "Все" },
+    { id: "Administrator", label: "Администратор" },
+    { id: "Accountant", label: "Бухгалтер" },
+    { id: "Repairman", label: "Ремонтник" },
+    { id: "Cashier", label: "Кассир" }
+  ],
+  /*function(){
+   let allRolesAr = roles*//*this.get('roles')*//*;
+   //allRolesAr.push({id: 0, label: "Все"});
+   return allRolesAr;
+   },*/
+
+  //queryParams defaults
+  login: '',
+  role: '',
+  lastName: '',
+  firstName: '',
+  middleName: '',
+
+  //variables for filter forms on templates
+  filterLastName: '',
+  filterFirstName: '',
+  filterMiddleName: '',
+  filterRole: '',/*{ id: 0, label: "Все" },*/
+  filterLogin: '',
+
   selectionEmpty: function(){
     return this.get("selectedRow") == null;
   }.property("selectedRow"),
-
   actions: {
     edit: function(){
       let row = this.get("selectedRow");
@@ -23,6 +57,40 @@ export default Ember.ArrayController.extend({
 		row.deleteRecord();
 		row.save();
 	  }
+    },
+    filterTable: function(){
+      let filterLastName = this.get('filterLastName');
+      let filterFirstName = this.get('filterFirstName');
+      let filterMiddleName = this.get('filterMiddleName');
+      let filterRole = this.get('filterRole.id');
+      let filterLogin = this.get('filterLogin');
+      let filter = {};
+      filter.lastName = filterLastName;
+      filter.firstName = filterFirstName;
+      filter.middleName = filterMiddleName;
+      filter.login = filterLogin;
+      filter.role = filterRole;
+
+      for (var prop in filter){
+        if (!filter[prop]){
+          filter[prop] = '';
+          this.set(prop, '');
+        }
+      }
+      this.transitionToRoute("users", {queryParams: filter});
+    },
+    clearFilter: function(){
+      this.set('filterLastName','');
+      this.set('filterFirstName','');
+      this.set('filterMiddleName','');
+      this.set('filterRole','');
+      this.set('filterLogin','');
+      this.set('lastName','');
+      this.set('firstName','');
+      this.set('middleName','');
+      this.set('role', '');
+      this.set('login','');
+      this.transitionToRoute("users", {queryParams: null});
     }
   },
 
--- sources/client/app/routes/users/index.js
+++ sources/client/app/routes/users/index.js
@@ -1,7 +1,45 @@
 import ProtectedRoute from "client/routes/base/protected";
 
 export default ProtectedRoute.extend({
-  model: function() {
-    return this.store.find("user");
+  queryParams: {
+    firstName:{
+      refreshModel: true
+    },
+    lastName:{
+      refreshModel: true
+    },
+    middleName:{
+      refreshModel: true
+    },
+    login: {
+      refreshModel: true
+    },
+    role:{
+      refreshModel: true
+    }
+  },
+  model: function(params) {
+    for(var prop in params){
+      if (params.hasOwnProperty(prop) && !params[prop]){
+        delete params[prop];
+      }
+    }
+    // This gets called upon entering 'articles' route
+    // for the first time, and we opt into refiring it upon
+    // query param changes by setting `refreshModel:true` above.
+
+    // params has format of { category: "someValueOrJustNull" },
+    // which we can just forward to the server.
+    return this.store.find('user', params);
+  },
+  resetController: function (controller, isExiting) {
+    if (isExiting) {
+      if (isExiting) {
+        var queryParams = controller.get('queryParams');
+        for (var i = 0; i < queryParams.length; i++) {
+          controller.set(queryParams[i], '');
+        }
+      }
+    }
   }
 });
