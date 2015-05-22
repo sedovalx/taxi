aa02f3b345bb034d3022ebc087e4fdc775b54c96
Argelein <argelein@gmail.com>
2015-05-22 13:11:15 +0300

    #23: Исправил опечатку в роли администратора.

--- sources/client/app/models/roles.js
+++ sources/client/app/models/roles.js
@@ -1,5 +1,5 @@
 export default [
-  { id: "Administrator", label: "Администритор" },
+  { id: "Administrator", label: "Администратор" },
   { id: "Accountant", label: "Бухгалтер" },
   { id: "Repairman", label: "Ремонтник" },
   { id: "Cashier", label: "Кассир" }
