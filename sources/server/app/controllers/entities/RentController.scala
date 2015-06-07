d319b4000c80d9c8062c691a738f26ad2bd8ed46
sedovalx <zaq1xsw2>
2015-06-08 01:32:49 +0300

    #76: контроллер аренд готов отдавать полное описание аренды при запросе по id

--- sources/server/app/controllers/entities/RentController.scala
+++ sources/server/app/controllers/entities/RentController.scala
@@ -3,11 +3,16 @@ package controllers.entities
 import models.entities.RentStatus.RentStatus
 import models.generated.Tables
 import models.generated.Tables.{Account, Rent, RentFilter, RentTable}
+import play.api.Logger
 import play.api.libs.json._
 import repository.RentRepo
 import scaldi.Injector
 import serialization.RentSerializer
 import service.{CarService, DriverService, RentService}
+import play.api.libs.functional.syntax._
+import play.api.libs.json.Reads._
+
+import scala.language.postfixOps
 
 class RentController(implicit injector: Injector) extends EntityController[Rent, RentTable, RentRepo, RentFilter, RentSerializer]()(injector) {
   override protected val entityService = inject [RentService]
@@ -49,7 +54,25 @@ class RentController(implicit injector: Injector) extends EntityController[Rent,
         val status = entityService.getCurrentStatus(r)
         val carName = carService.getDisplayName(r.carId)
         val driverName = driverService.getDisplayName(r.driverId)
-        json
+
+        // без внешних скобок это шаманство не работает
+        val transform = (
+            (__ \ "rent").json.pickBranch and
+            (__ \ "rent").json.pickBranch(
+              (__ \ "status").json.put(JsString(status.toString)) and
+              (__ \ "carDisplayName").json.put(JsString(carName)) and
+              (__ \ "driverDisplayName").json.put(JsString(driverName))
+                reduce
+            ) reduce
+          )
+
+        json.transform(transform) match {
+          case JsSuccess(obj, _) => obj
+          case JsError(errors) =>
+            Logger.warn(s"Rent serialization error: $errors")
+            json
+        }
+
     }
   }
 }
