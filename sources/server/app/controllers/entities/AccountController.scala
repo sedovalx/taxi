dbd5af7cd50d49f1f6aa1079ab156b704e9c7470
sedovalx <zaq1xsw2>
2015-06-07 03:34:04 +0300

    #76: отфилььрованный по аренде список платежей отображается из редактора аренды. На сервере рефакторинг сериализации.

--- sources/server/app/controllers/entities/AccountController.scala
+++ sources/server/app/controllers/entities/AccountController.scala
@@ -1,80 +1,27 @@
 package controllers.entities
 
-import java.sql.Timestamp
-
-import models.entities.Role
-import models.entities.Role._
-import models.generated.Tables
-import models.generated.Tables.{AccountFilter, Account, AccountTable}
-import play.api.libs.functional.syntax._
-import play.api.libs.json.Reads._
+import models.generated.Tables.{Account, AccountFilter, AccountTable}
 import play.api.libs.json._
 import repository.AccountRepo
 import scaldi.Injector
+import serialization.AccountSerializer
 import service.AccountService
-import utils.serialization.EnumSerializer
 
-class AccountController(implicit inj: Injector) extends EntityController[Account, AccountTable, AccountRepo, AccountFilter]()(inj) {
+class AccountController(implicit inj: Injector)
+  extends EntityController[Account, AccountTable, AccountRepo, AccountFilter, AccountSerializer]()(inj) {
   protected val entityService = inject [AccountService]
+  override protected val serializer: AccountSerializer = inject [AccountSerializer]
+
+  import serializer._
 
   protected val entitiesName: String = "users"
   protected val entityName: String = "user"
-
-  private implicit val enumReads = EnumSerializer.enumReads(Role)
-  private implicit val enumWrites = EnumSerializer.enumWrites
 
   override protected def beforeCreate(json: JsValue, entity: Account, identity: Account) = {
     val user = super.beforeCreate(json, entity, identity)
     require(user.passwordHash != null, "Пароль пользователя не должен быть пустым.")
     user
   }
-
-  protected implicit val reads: Reads[Tables.Account] = (
-    (JsPath \ "id").readNullable[String].map { case Some(s) => s.toInt case None => 0 } and
-      (JsPath \ "login").read(minLength[String](3)) and
-      (JsPath \ "password").readNullable(minLength[String](8)).map(o => o.orNull) and
-      (JsPath \ "lastName").readNullable[String] and
-      (JsPath \ "firstName").readNullable[String] and
-      (JsPath \ "middleName").readNullable[String] and
-      (JsPath \ "role").read[Role] and
-      (JsPath \ "comment").readNullable[String] and
-      (JsPath \ "creationDate").readNullable[Timestamp] and
-      (JsPath \ "editDate").readNullable[Timestamp] and
-      (JsPath \ "creator").readNullable[String].map { s => s.map(_.toInt) } and
-      (JsPath \ "editor").readNullable[String].map { s => s.map(_.toInt) }
-    )(Account.apply _)
-
-  protected implicit val writes: Writes[Tables.Account] = new Writes[Account] {
-    def writes(o: Account) = Json.obj(
-      "id" -> o.id.toString,
-      "login" -> o.login,
-      "password" -> JsNull,
-      "lastName" -> o.lastName,
-      "firstName" -> o.firstName,
-      "middleName" -> o.middleName,
-      "role" -> o.role.toString,
-      "creationDate" -> o.creationDate.map { d => dateIso8601Format.format(d)} ,
-      "editDate" -> o.editDate.map { d => dateIso8601Format.format(d)},
-      "creator" -> o.creatorId.map { id => id.toString },
-      "editor" -> o.editorId.map { id => id.toString },
-      "comment" -> o.comment
-    )
-  }
-
-  override protected implicit val filterReads: Reads[Tables.AccountFilter] = (
-    (JsPath \ "id").readNullable[String].map { s => s.map(_.toInt) } and
-      (JsPath \ "login").readNullable[String] and
-      (JsPath \ "password").readNullable[String] and
-      (JsPath \ "lastName").readNullable[String] and
-      (JsPath \ "firstName").readNullable[String] and
-      (JsPath \ "middleName").readNullable[String] and
-      (JsPath \ "role").readNullable[Role] and
-      (JsPath \ "comment").readNullable[String] and
-      (JsPath \ "creationDate").readNullable[Timestamp] and
-      (JsPath \ "editDate").readNullable[Timestamp] and
-      (JsPath \ "creator").readNullable[String].map { s => s.map(_.toInt) } and
-      (JsPath \ "editor").readNullable[String].map { s => s.map(_.toInt) }
-    )(AccountFilter.apply _)
 
   def currentUser = SecuredAction { request =>
     val user = request.identity
