03dce75e6b8e19f26e47436c87c510cd763f9c29
sedovalx <zaq1xsw2>
2015-06-14 23:47:26 +0300

    #66, #67: конфиг для Silhouette

---
+++ sources/server/app/config/SilhouetteModule.scala
@@ -0,0 +1,43 @@
+package config
+
+import com.google.inject.{AbstractModule, Provides}
+import com.mohiva.play.silhouette.api.util.Clock
+import com.mohiva.play.silhouette.api.{Environment, EventBus}
+import com.mohiva.play.silhouette.impl.authenticators.{JWTAuthenticator, JWTAuthenticatorService, JWTAuthenticatorSettings}
+import com.mohiva.play.silhouette.impl.util.SecureRandomIDGenerator
+import models.generated.Tables.SystemUser
+import net.codingwell.scalaguice.ScalaModule
+import play.api.Play
+import service.{LoginInfoService, LoginInfoServiceImpl}
+
+class SilhouetteModule extends AbstractModule with ScalaModule {
+  override def configure(): Unit = {
+    bind[LoginInfoService].to[LoginInfoServiceImpl]
+  }
+
+  @Provides
+  def provideSettings(): JWTAuthenticatorSettings = {
+    JWTAuthenticatorSettings(
+      headerName = Play.configuration.getString("silhouette.authenticator.headerName").getOrElse { "X-Auth-Token" },
+      issuerClaim = Play.configuration.getString("silhouette.authenticator.issueClaim").getOrElse { "play-silhouette" },
+      encryptSubject = Play.configuration.getBoolean("silhouette.authenticator.encryptSubject").getOrElse { true },
+      authenticatorIdleTimeout = Play.configuration.getInt("silhouette.authenticator.authenticatorIdleTimeout"),
+      authenticatorExpiry = Play.configuration.getInt("silhouette.authenticator.authenticatorExpiry").getOrElse { 12 * 60 * 60 },
+      sharedSecret = Play.configuration.getString("application.secret").get
+    )
+  }
+
+  @Provides
+  def provideAuthenticatorService(settings: JWTAuthenticatorSettings): JWTAuthenticatorService = {
+    new JWTAuthenticatorService(settings, None, new SecureRandomIDGenerator, Clock())
+  }
+
+  @Provides
+  def provideEnvironment(
+    userService: LoginInfoService,
+    authenticatorService: JWTAuthenticatorService,
+    eventBus: EventBus): Environment[SystemUser, JWTAuthenticator] = {
+
+    Environment(userService, authenticatorService, Seq(), eventBus)
+  }
+}
