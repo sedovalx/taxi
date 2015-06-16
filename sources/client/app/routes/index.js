a985ef5db7a6856f768b8a1ec808b19a006e9892
sedovalx <zaq1xsw2>
2015-06-17 01:58:03 +0300

    сделан переход на кассовыую форму по умолчанию при старте приложения в корне

--- sources/client/app/routes/index.js
+++ sources/client/app/routes/index.js
@@ -3,6 +3,6 @@ import Ember from "ember";
 export default Ember.Route.extend({
   beforeModel: function(){
     // при переходе в корень сайта делаем редирект на данный список
-    this.transitionTo("users");
+    this.transitionTo("cashier-list");
   }
 });
