44e6c1311d8c1bdde3a31167b16790fbfa9183cb
sedovalx <sedov.alx@gmail.com>
2015-07-17 02:49:58 +0300

    Fixed #100: время уходит корректное, контрол заменен

--- sources/client/app/components/datetime-picker.js
+++ sources/client/app/components/datetime-picker.js
@@ -3,91 +3,40 @@ import $ from 'jquery';
 /* global moment */
 // No import for moment, it's a global called `moment`
 
-
 export default Ember.Component.extend({
-  layoutName: 'ember-bootstrap-datetimepicker-template-main',
-  init: function(){
-    // initialize pickerValue with bound value property.
-    this.set('pickerValue', this.formatTimestamp(this.get('value')));
-    this._super();
-  },
-  dateFormat: 'MM/DD/YYYY hh:mm',
-  formatTimestamp: function(timestamp){
-    if(timestamp){
-      return moment(timestamp).format(this.get('dateFormat'));
-    }else{
-      return null;
-    }
-  },
-  dateStringToTimestamp: function(dateString){
-    if(dateString){
-      return moment(dateString, this.get('dateFormat')).unix() * 1000;
-    }else{
-      return null;
-    }
-  },
-  classNames: ['datepicker-component'],
-  $el: null,
-  $datepicker: null,
-  useCurrent: true,
-  minDate: false,
-  maxDate: false,
-  showTodayButton: true,
-  locale: 'en',
-  defaultDate: null,
-  disabledDates: [],
-  enabledDates: [],
-  useStrict: false,
-  sideBySide: false,
-  daysOfWeekDisabled: [],
+  value: null,
+  format: 'DD.MM.YYYY HH:mm:ss',
+  locale: 'ru',
+  allowInputToggle: true,
   icons: {
     time: 'fa fa-clock-o',
-    calendar: 'fa fa-calendar',
+    date: 'fa fa-calendar',
     up: 'fa fa-chevron-up',
-    down: 'fa fa-chevron-down'
+    down: 'fa fa-chevron-down',
+    previous: 'fa fa-chevron-left',
+    next: 'fa fa-chevron-right',
+    today: 'fa fa-dot-circle-o',
+    clear: 'fa fa-trash-o',
+    close: 'fa fa-times'
   },
-  value: null,
-  pickerOpen: false,
+  init: function(){
+    let value = this.get('value') || new Date();
+    this.set('initValue', moment(value).format(this.get('format')));
+    this._super();
+  },
   didInsertElement: function(){
-    var self = this,
-      elmId = this.get('elementId'),
-      $dp = $('#'+elmId+' .datepicker-input'),
-      $calIcon = $('#'+elmId+' .calendar-button');
-    if($dp.length > 0){
-      $dp.datetimepicker({
-        widgetParent: '#'+elmId,
-        useCurrent: this.get('useCurrent'),
-        minDate: this.get('minDate'),
-        maxDate: this.get('maxDate'),
-        showTodayButton: this.get('showTodayButton'),
-        locale: this.get('locale'),
-        defaultDate: this.get('defaultDate'),
-        disabledDates: this.get('disabledDates'),
-        enabledDates: this.get('enabledDates'),
-        useStrict: this.get('useStrict'),
-        sideBySide: this.get('sideBySide'),
-        daysOfWeekDisabled: this.get('daysOfWeekDisabled'),
-        icons: this.get('icons')
-      });
-      $dp.on('dp.change', function(){
-        self.set('value', self.dateStringToTimestamp($dp.val()));
-      });
-      $dp.on('dp.show', function(){
-        self.set('pickerOpen', true);
-      });
-      $dp.on('dp.hide', function(){
-        self.set('pickerOpen', false);
-      });
-      $calIcon.on('click', function(){
-        if(self.get('pickerOpen')){
-          $dp.data('DateTimePicker').hide();
-          self.set('pickerOpen', false);
-        }else{
-          $dp.data('DateTimePicker').show();
-          self.set('pickerOpen', true);
-        }
-      });
-      this.set('$datepicker', $dp);
-    }
+    let self = this;
+    let elementId = this.get('elementId');
+    let $picker = $('#' + elementId + ' .input-group');
+    $picker.datetimepicker({
+      format: this.get('format'),
+      locale: this.get('locale'),
+      icons: this.get('icons'),
+      allowInputToggle: this.get('allowInputToggle')
+    });
+    $picker.on('dp.change', function(){
+      let momentDate = $picker.data('DateTimePicker').date();
+      self.set('value', momentDate.toDate());
+    });
   }
 });
