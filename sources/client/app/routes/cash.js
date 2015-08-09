ea88234bc3ed3e7f381d588aa4d6f90f0fc02a9c
sedovalx <sedov.alx@gmail.com>
2015-08-09 23:37:38 +0300

    #91: снятие денег из кассы реализовано на клиенте

--- sources/client/app/routes/cash.js
+++ sources/client/app/routes/cash.js
@@ -1,7 +1,20 @@
 import ProtectedRoute from "client/routes/base/protected";
+import DirtyRouteMixin from "client/routes/base/dirty-route-mixin";
+import Ember from "ember";
 
-export default ProtectedRoute.extend({
+export default ProtectedRoute.extend(DirtyRouteMixin, {
   model: function(){
-    return this.store.createRecord("profit");
+    return Ember.$.getJSON('/api/profits/current').then(response => {
+        return {
+          currentAmount: response.amount,
+          model: this.store.createRecord("profit")
+        };
+      }
+    );
+  },
+  setupController: function(controller, data) {
+    data.model.set('amount', data.currentAmount);
+    controller.set('model', data.model);
+    controller.set('currentAmount', data.currentAmount);
   }
 });
