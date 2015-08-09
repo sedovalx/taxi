af36c8b8c770f7aea76c2737ef666ae618eb66ea
sedovalx <sedov.alx@gmail.com>
2015-08-09 22:57:31 +0300

    #91: добавлены серверные маршруты для получения суммы в кассе

--- sources/server/app/controllers/api/entity/ProfitController.scala
+++ sources/server/app/controllers/api/entity/ProfitController.scala
@@ -7,9 +7,12 @@ import com.mohiva.play.silhouette.impl.authenticators.JWTAuthenticator
 import models.generated.Tables
 import models.generated.Tables.{Profit, ProfitFilter, ProfitTable}
 import play.api.i18n.MessagesApi
+import play.api.libs.json.Json
 import repository.ProfitRepo
 import serialization.entity.ProfitSerializer
 import service.entity.ProfitService
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class ProfitController @Inject()(
   val messagesApi: MessagesApi,
@@ -19,4 +22,12 @@ class ProfitController @Inject()(
   extends EntityController[Profit, ProfitTable, ProfitRepo, ProfitFilter, ProfitSerializer] {
   override protected val entityName: String = "profit"
   override protected val entitiesName: String = "profits"
+
+  def currentState = SecuredAction.async { implicit request =>
+    logger.debug(s"Запрос на получение суммы в кассе: $request")
+    entityService.getCurrentState map { amount =>
+      logger.debug(s"Ответ на запрос суммы в кассе: $amount")
+      Ok(Json.obj("amount" -> amount))
+    }
+  }
 }
--- sources/server/app/service/entity/ProfitService.scala
+++ sources/server/app/service/entity/ProfitService.scala
@@ -9,7 +9,9 @@ import slick.driver.JdbcProfile
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 
-trait ProfitService extends EntityService[Profit, ProfitTable, ProfitRepo, ProfitFilter]
+trait ProfitService extends EntityService[Profit, ProfitTable, ProfitRepo, ProfitFilter]{
+  def getCurrentState: Future[BigDecimal]
+}
 
 class ProfitServiceImpl @Inject() (
   val repo: ProfitRepo,
@@ -20,7 +22,7 @@ class ProfitServiceImpl @Inject() (
   import dbConfig.driver.api._
 
   override protected def beforeCreate(entity: Profit, creatorId: Option[Int]): Future[Profit] = {
-    super.beforeUpdate(entity, creatorId) flatMap { entity =>
+    super.beforeCreate(entity, creatorId) flatMap { entity =>
       checkAmount(entity)
     }
   }
@@ -31,25 +33,33 @@ class ProfitServiceImpl @Inject() (
     }
   }
 
-  private def checkAmount(entity: Profit): Future[Profit] = {
+  def getCurrentState: Future[BigDecimal] = {
     val zero = BigDecimal(0)
+    // дата последнего снятия кассы
     val lastProfitQuery = repo.tableQuery.sortBy(p => p.changeTime.desc).take(1).map(p => p.changeTime)
     dbConfig.db.run(lastProfitQuery.result) flatMap { dates =>
+      // сумма положительных операций после последнего съема кассы
       val operationQuery = (dates.headOption match {
         case Some(lastProfitTime) => operationRepo.tableQuery.filter(o => o.changeTime > lastProfitTime)
         case None => operationRepo.tableQuery
       }).filter(o => o.amount > zero).map(o => o.amount).sum
 
+      // сумма возвратов после последнего съема кассы
       val refundQuery = (dates.headOption match {
         case Some(lastProfitTime) => refundRepo.tableQuery.filter(r => r.changeTime > lastProfitTime)
         case None => refundRepo.tableQuery
       }).map(r => r.amount).sum
 
+      // расчетная сумма в кассе
       for {
         operations <- dbConfig.db.run(operationQuery.result) map { v => v.getOrElse(BigDecimal(0)) }
         refunds <- dbConfig.db.run(refundQuery.result) map { v => v.getOrElse(BigDecimal(0))}
       } yield operations - refunds
-    } map { amount =>
+    }
+  }
+
+  private def checkAmount(entity: Profit): Future[Profit] = {
+    getCurrentState map { amount =>
       entity.amount match {
         case Some(x) if x > amount => throw new RuntimeException(s"Невозможно изъять из кассы более $amount")
         case None => entity.copy(amount = Some(amount))
